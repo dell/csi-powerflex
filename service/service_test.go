55b654de36a530f89444ec1d680a550588016c44
nb950 <foth2012@gmail.com>
2021-11-22 22:13:14 -0500

    run testing before

--- service/service_test.go
+++ service/service_test.go
@@ -15,25 +15,27 @@ func TestMain(m *testing.M) {
 	go http.ListenAndServe("localhost:6060", nil)
 	fmt.Printf("starting godog...\n")
 
+	status := m.Run()
+
+	fmt.Printf("unit test status %d\n", status)
+
 	opts := godog.Options{
 		Format: "pretty",
 		Paths:  []string{"features"},
 		//Tags:   "wip",
 	}
 
-	status := godog.TestSuite{
+	st := godog.TestSuite{
 		Name:                "godog",
 		ScenarioInitializer: FeatureContext,
 		Options:             &opts,
 	}.Run()
 
-	fmt.Printf("godog finished\n")
-
-	if st := m.Run(); st > status {
+	if st > status {
 		status = st
 	}
 
-	fmt.Printf("status %d\n", status)
+	fmt.Printf("godog test status %d\n", status)
 
 	os.Exit(status)
 }
