41207e3026eb44a41b97d79246479614ad9febd8
JacobGros <jacobgrosner4@gmail.com>
2022-05-13 11:36:28 -0400

    gofmt fix

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -86,9 +86,9 @@ func TestMain(m *testing.M) {
 	if file != nil {
 		file.Close()
 	}
-	
+
 	outputfile, err := os.Create("integration.xml")
-	
+
 	opts := godog.Options{
 		Output: outputfile,
 		Format: "junit",
