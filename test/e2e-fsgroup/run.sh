fe00f8d5d4d5ef34096ad07fb8edc8edd9065676
JacobGros <jacobgrosner4@gmail.com>
2022-05-20 16:26:58 -0400

    run.sh edits

--- test/e2e-fsgroup/run.sh
+++ test/e2e-fsgroup/run.sh
@@ -2,14 +2,14 @@
 # supress ginkgo 2.0 upgrade hints
 export ACK_GINKGO_DEPRECATIONS=1.16.5
 
-# another way to run tests
-#go test -timeout=105s -v ./ -ginkgo.v=1
+# run all tests 
+go test -timeout=25m -v ./ -ginkgo.v=1
 
 # use focus to run only one test from fs_scaleup_scaledown.go
 #ginkgo -mod=mod --focus=Scale ./...
 
-# use focus to run only one test from fs.go
-ginkgo -mod=mod --focus=FSGroup --timeout=25m ./...
+# use focus to run only certain tests
+#ginkgo -mod=mod --focus=FSGroup --timeout=25m ./...
 
 # run ephemeral only test
-ginkgo -mod=mod --focus=Ephemeral ./...
+#ginkgo -mod=mod --focus=Ephemeral ./...
