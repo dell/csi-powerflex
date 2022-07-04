141eef484535cd1dcfcd24c169bff4c673b07d7d
Fernando Alfaro Campos <Fernando.Alfaro_campos@Dell.com>
2022-07-04 16:10:22 -0400

    Update csi endpoint to be valid in sanity run script

--- test/sanity/run.sh
+++ test/sanity/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf /tmp/csi-staging
 rm -rf /tmp/csi-mount
-csi-sanity --ginkgo.v --csi.endpoint=/root/csi-vxflexos/test/sanity/unix_sock --csi.testvolumeexpandsize 25769803776 --csi.testvolumesize 17179869184 --csi.testvolumeparameters=volParams.yaml --csi.secrets=secrets.yaml --ginkgo.skip "pagination should detect volumes added between pages and accept tokens when the last volume from a page is deleted|check the presence of new volumes and absence of deleted ones in the volume list|should fail when the volume is missing"
+
+csi-sanity --ginkgo.v --csi.endpoint=./unix_sock --csi.testvolumeexpandsize 25769803776 --csi.testvolumesize 17179869184 --csi.testvolumeparameters=volParams.yaml --csi.secrets=secrets.yaml --ginkgo.skip "pagination should detect volumes added between pages and accept tokens when the last volume from a page is deleted|check the presence of new volumes and absence of deleted ones in the volume list|should fail when the volume is missing"
