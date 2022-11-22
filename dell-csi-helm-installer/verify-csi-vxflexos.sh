543508c3ec9d9ef87dbaad1e160ddf778ca9c908
Ashley_Joy <Ashley.Joy@dell.com>
2022-11-22 14:08:51 +0530

    k8s 1.25 verification and update go version in go.mod

--- dell-csi-helm-installer/verify-csi-vxflexos.sh
+++ dell-csi-helm-installer/verify-csi-vxflexos.sh
@@ -14,7 +14,7 @@
 
 # verify-csi-vxflexos method
 function verify-csi-vxflexos() {
-  verify_k8s_versions "1.21" "1.24"
+  verify_k8s_versions "1.21" "1.25"
   verify_openshift_versions "4.10" "4.11"
   verify_namespace "${NS}"
   verify_helm_values_version "${DRIVER_VERSION}"
