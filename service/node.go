82c794e7d121e88ac2c9ee19afe4391be4291717
VamsiSiddu-7 <vamsikrishna.siddu@dell.com>
2023-02-16 08:02:04 -0500

    add more debugs.

--- service/node.go
+++ service/node.go
@@ -477,12 +477,12 @@ func (s *service) renameSDC(opts Opts) error {
 
 	// fetch SDC details
 	for _, systemID := range connectedSystemID {
-		fmt.Println("systemID", systemID)
+		fmt.Println("******systemID********", systemID)
 		system := s.systems[systemID]
-		fmt.Println("system", system)
-		fmt.Println("SdcGuid", opts.SdcGUID)
+		fmt.Println("*******system*********", system)
+		fmt.Println("******SdcGuid********", opts.SdcGUID)
 		sdc, err := system.FindSdc("SdcGUID", opts.SdcGUID)
-		fmt.Println("sdc ", sdc)
+		fmt.Println("********sdc**********", sdc)
 		if err != nil {
 			return status.Errorf(codes.FailedPrecondition, "%s", err)
 		}
