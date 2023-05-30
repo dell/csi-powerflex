0f378fe31532dc93abf05164c74a8c960227511e
Vamsikrishna_Siddu <vamsikrishna.siddu@dell.com>
2023-05-30 02:16:44 -0400

    fixed the file build_ubi_micro.sh

--- scripts/build_ubi_micro.sh
+++ scripts/build_ubi_micro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-microcontainer=$(buildah from registry.access.redhat.com/ubi8/ubi-micro)
+microcontainer=$(buildah from $1)
 micromount=$(buildah mount $microcontainer)
 dnf install --installroot $micromount --releasever=8 --nodocs --setopt install_weak_deps=false --setopt=reposdir=/etc/yum.repos.d/ e4fsprogs xfsprogs libaio kmod numactl util-linux -y
 dnf clean all --installroot $micromount
