9fdc23f313f6f1b53a128803de272927932176df
Vamsikrishna_Siddu <vamsikrishna.siddu@dell.com>
2023-06-05 00:44:59 -0400

    changed the go version to 1.20.4

--- overrides.mk
+++ overrides.mk
@@ -4,7 +4,7 @@
 
 # DEFAULT values
 DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi8/ubi-micro:8.7-8"
-DEFAULT_GOVERSION="1.20"
+DEFAULT_GOVERSION="1.20.4"
 DEFAULT_REGISTRY="sample_registry"
 DEFAULT_IMAGENAME="csi-vxflexos"
 DEFAULT_BUILDSTAGE="final"
