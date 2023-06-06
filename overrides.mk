3825a766114af792b6a94236d2b6865493d9860e
VamsiSiddu-7 <103578883+VamsiSiddu-7@users.noreply.github.com>
2023-06-06 11:14:33 +0530

    update the redhat ubi-micro image to latest 8.8-1 (#206)
    * updated the ubi-micro image to latest.
    
    * add copyright header.

--- overrides.mk
+++ overrides.mk
@@ -3,7 +3,8 @@
 #
 
 # DEFAULT values
-DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi8/ubi-micro:8.7-8"
+# ubi8/ubi-micro:8.8-1
+DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi8/ubi-micro:8.8-1"
 DEFAULT_GOVERSION="1.20"
 DEFAULT_REGISTRY="sample_registry"
 DEFAULT_IMAGENAME="csi-vxflexos"
