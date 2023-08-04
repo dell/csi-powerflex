593f39abc5a0f774554c56d3638ba6c51a812055
Vamsikrishna_Siddu <vamsikrishna.siddu@dell.com>
2023-08-04 05:12:42 -0400

    add support for RWX.

--- service/service.go
+++ service/service.go
@@ -1181,7 +1181,6 @@ func (s *service) exportFilesystem(ctx context.Context, req *csi.ControllerPubli
 	otherHostsWithAccess += len(readOnlyRootHosts)
 	if !foundIncompatible {
 		for _, host := range readOnlyRootHosts {
-			readHostList = append(readHostList, host)
 			if host == hostURL {
 				if am.Mode == csi.VolumeCapability_AccessMode_MULTI_NODE_READER_ONLY {
 					foundIdempotent = true
@@ -1195,7 +1194,6 @@ func (s *service) exportFilesystem(ctx context.Context, req *csi.ControllerPubli
 
 	if !foundIncompatible && !foundIdempotent {
 		for _, host := range readWriteRootHosts {
-			readWriteHostList = append(readWriteHostList, hostURL)
 			if host == hostURL {
 				if am.Mode == csi.VolumeCapability_AccessMode_MULTI_NODE_READER_ONLY {
 					foundIncompatible = true
