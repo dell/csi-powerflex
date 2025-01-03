e5725cc5f744dd4a5dde3e1fbc044f29d8476f8e
Rishabh Raj <120644626+rishabhatdell@users.noreply.github.com>
2025-01-03 11:26:57 +0530

    Update K8s & OCP Version

--- dell-csi-helm-installer/verify-csi-vxflexos.sh
+++ dell-csi-helm-installer/verify-csi-vxflexos.sh
@@ -14,8 +14,8 @@
 
 # verify-csi-vxflexos method
 function verify-csi-vxflexos() {
-  verify_k8s_versions "1.25" "1.31"
-  verify_openshift_versions "4.16" "4.17"
+  verify_k8s_versions "1.26" "1.32"
+  verify_openshift_versions "4.17" "4.18"
   verify_namespace "${NS}"
   verify_helm_values_version "${DRIVER_VERSION}"
   verify_required_secrets "${RELEASE}-config"
